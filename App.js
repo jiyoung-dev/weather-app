925d99a42ab039b9979e15d118d7a01d13992122
Jiyoung <janice10508@gmail.com>
2021-05-23 12:49:52 +0900

    Weather API사용, 날씨data불러오기-expo icon연동-1차화면구현 ok

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert} from "react-native";
+import Loading from "./Loading";
+import * as Location from 'expo-location';
+import axios from "axios";
+import Weather from "./Weather";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "f0f6390c45c893b352d575905fc41b22";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  // 날씨정보 가져오기 
+  getWeather = async(latitude, longitude) => {
+    const { data: {
+      main: {temp}, 
+      weather
+    } 
+  } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    // console.log(data);
+    this.setState({ 
+      isLoading: false, 
+      condition: weather[0].main,
+      temp
+    });
+  };
+  // 위치정보 가져오기 
+  getLocation = async() => {
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude)
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    // 로딩중이라면 Loading화면 리턴, 아니면 null
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition}/>;  
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
